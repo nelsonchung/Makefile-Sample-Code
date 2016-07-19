f3ae843afa97e28e0b67542d67db5790cdc2c08e
nelsonchung <chihchun.chung@gmail.com>
2016-07-19 17:21:23 +0800

    Add several sample codes.

---
+++ buildallc/app/app_hello/a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("I am a.c");
+    return;
+}
---
+++ buildlibrary/commonapi/staticlib/show_head.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void show_head(void)
+{
+    printf("show head");
+}
